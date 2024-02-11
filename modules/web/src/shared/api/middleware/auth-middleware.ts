5954351fa6e8b2236814da15d1ff9c2ffa33b328
bxr5xrk <vasyldobrovoslkyi@gmail.com>
2024-02-11 18:45:41 +0200

    fix[web]: fixed auth middleware

--- modules/web/src/shared/api/middleware/auth-middleware.ts
+++ modules/web/src/shared/api/middleware/auth-middleware.ts
@@ -4,7 +4,7 @@ import { Middleware, SWRHook } from "swr";
 export const authMiddleware: Middleware = (useSWRNext: SWRHook) => (key, fetcher, config) => {
     const { data, error } = useWhoami();
 
-    const isLoggedIn = data?.id && !error;
+    const isLoggedIn = data?.data.id && !error;
 
     if (!isLoggedIn) {
         return useSWRNext(null, fetcher, config);
