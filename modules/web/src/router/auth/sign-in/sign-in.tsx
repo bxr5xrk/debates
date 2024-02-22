96206e007151913b5dd69d660fae850d86d3854a
thisisnago <yurii.nagorniak@gmail.com>
2024-02-22 00:20:57 +0000

    Added tablet and desktop version of login page

--- modules/web/src/router/auth/sign-in/sign-in.tsx
+++ modules/web/src/router/auth/sign-in/sign-in.tsx
@@ -5,7 +5,7 @@ export function SignInPage(): JSX.Element {
 
     return (
         <Page className="flex items-center justify-center">
-            <div className="max-w-full p-4">
+            <div className="w-full sm:w-80 lg:w-full p-4">
                 <SignInByCredentialsForm />
             </div>
         </Page>
