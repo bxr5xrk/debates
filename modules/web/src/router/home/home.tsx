81fc35971e62773ca37b3ace937722cda926a642
OlegKryvohub <2103kokakola2004@gmail.com>
2024-02-26 02:18:26 +0200

    fix bug and add responsiv

--- modules/web/src/router/home/home.tsx
+++ modules/web/src/router/home/home.tsx
@@ -1,18 +1,31 @@
+"use client";
 import React from "react";
 import { Page } from "@/shared/layout/page";
+
 import { HomePrivate } from "./ui/home-private/home-private";
 import { HomePublic } from "./ui/home-public/home-public";
+
 import { Text } from "@/shared/ui";
 import { Button } from "@/shared/ui";
+
 import Image from "next/image";
 
+import { useRouter } from "next/navigation";
+
 export function HomePage(): JSX.Element {
+    const router = useRouter();
+
+    const handleButtonClick = (navigate: string): void => {
+        router.push(`/${navigate}`);
+    };
+
     return (
         <Page className="p-8">
             <HomePrivate />
             <HomePublic />
             <div className="flex">
                 <Button
+                    onClick={() => handleButtonClick("sign-up")}
                     className="ml-auto font-bold w-21 text-25 transition duration-1000
                     after:content-[''] after:hover:bg-blackLight hover:text-white"
                 >
@@ -29,12 +42,19 @@ export function HomePage(): JSX.Element {
                 />
                 <Text
                     Tag="h1"
-                    classes={["text-9xl", "font-bold", "text-center"]}
+                    classes={[
+                        "sm:text-7xl",
+                        "md:text-8xl",
+                        "lg:text-9xl",
+                        "font-bold",
+                        "text-center",
+                    ]}
                 >
                     DEBATE NIGHT
                 </Text>
                 <Button
-                    className="w-80 font-bold text-5xl 
+                    onClick={() => handleButtonClick("#")}
+                    className="max-w-80 w-full font-bold text-5xl 
                 mx-auto mt-11 p-2 transition duration-1000 after:hover:bg-blackLight
                 after:content-['']  hover:text-white after:transition"
                 >
