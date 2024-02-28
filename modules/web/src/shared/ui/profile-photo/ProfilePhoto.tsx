ecbc6f4d0a2c8e086b831034b0f6a90bf5c69541
thisisnago <yurii.nagorniak@gmail.com>
2024-02-28 01:08:31 +0000

    Added styles for the user info page; fixed bug with hover effect on the profilePhoto; changed base-button styles

--- modules/web/src/shared/ui/profile-photo/ProfilePhoto.tsx
+++ modules/web/src/shared/ui/profile-photo/ProfilePhoto.tsx
@@ -29,14 +29,15 @@ ProfilePhotoProps): JSX.Element {
     return (
         <div>
             <label
-                className="flex rounded-full justify-center cursor-pointer items-center w-full relative"
+                className="flex rounded-full justify-center cursor-pointer items-center w-full"
                 htmlFor="image"
             >
-                <span className="z-20 block absolute top-0 left-0 w-full h-full border-4 border-slate-700 rounded-full text-transparent text-center font-bold text-2xl uppercase ease-in-out duration-300 flex items-center justify-center hover:bg-slate-700 hover:text-white">
-                    {profilePhoto ? "Change photo" : "Add photo"}
-                </span>
                 <span className="w-48 h-48 z-15 group block relative w-3/5 border-4 border-slate-700 rounded-full bg-slate-700 ease-in-out duration-300 overflow-hidden sm:w-56 sm:h-56 lg:w-64 lg:h-64">
                     {/* <span className="z-20 block absolute top-2/4 left-2/4 -translate-y-1/2 -translate-x-1/2 text-transparent text-center font-bold text-2xl uppercase ease-in-out duration-300 group-hover:text-white"> */}
+                    <span className="z-20 block absolute top-0 left-0 w-full h-full rounded-full text-transparent text-center font-bold text-2xl uppercase ease-in-out duration-300 flex items-center justify-center hover:bg-slate-700 hover:text-white">
+                        {profilePhoto ? "Change photo" : "Add photo"}
+                    </span>
+
                     <Image
                         className="w-full h-full object-cover"
                         src={profilePhotoUrl(profilePhoto)}
