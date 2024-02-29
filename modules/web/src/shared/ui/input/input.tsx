b3587f06bc19b1f1df9ecfbfc8f468e67de2c1c0
thisisnago <yurii.nagorniak@gmail.com>
2024-02-29 14:32:01 +0000

    Added styles for the create room page

--- modules/web/src/shared/ui/input/input.tsx
+++ modules/web/src/shared/ui/input/input.tsx
@@ -1,20 +1,21 @@
 import { cl } from "@/shared/lib/cl";
 import { DetailedHTMLProps, HTMLAttributes, InputHTMLAttributes, forwardRef } from "react";
 
-type InputProps = DetailedHTMLProps<InputHTMLAttributes<HTMLInputElement>, HTMLInputElement>
+type InputProps = DetailedHTMLProps<InputHTMLAttributes<HTMLInputElement>, HTMLInputElement>;
 
-export const Input = forwardRef<HTMLInputElement, InputProps>(
-    function Input({ ...props }, ref) {
-        return (
-            <input
-                autoComplete={props.autoComplete ?? props.id ?? "off"}
-                className={cl("flex h-10 w-full rounded-md border border-input bg-background px-3 py-6 text-xl ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50", props.className)}
-                {...props}
-                ref={ref}
-            />
-        );
-    },
-);
+export const Input = forwardRef<HTMLInputElement, InputProps>(function Input({ ...props }, ref) {
+    return (
+        <input
+            autoComplete={props.autoComplete ?? props.id ?? "off"}
+            className={cl(
+                "flex h-10 w-full rounded-md border border-input bg-background px-3 py-6 text-xl ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none disabled:cursor-not-allowed disabled:opacity-50",
+                props.className
+            )}
+            {...props}
+            ref={ref}
+        />
+    );
+});
 
 Input.displayName = "Input";
 
@@ -22,14 +23,14 @@ interface InputGroupProps extends React.HTMLAttributes<HTMLDivElement> {
     children: React.ReactNode;
     label?: string;
     errorMessage?: string;
-    containerProps?: DetailedHTMLProps<HTMLAttributes<HTMLDivElement>, HTMLDivElement>
+    containerProps?: DetailedHTMLProps<HTMLAttributes<HTMLDivElement>, HTMLDivElement>;
 }
 
 export function InputGroup(props: InputGroupProps): JSX.Element {
     const { label, children, id, errorMessage, containerProps, ...meta } = props;
 
     return (
-        <div {...meta} >
+        <div {...meta}>
             {label && <label htmlFor={id}>{label}</label>}
             <div {...containerProps} className={cl("border rounded-lg border-slate-700 p-2", containerProps?.className)}>
                 {children}
