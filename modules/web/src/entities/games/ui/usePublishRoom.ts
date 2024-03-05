0064e79135c0d963eca3bde4e146ed26a1259a32
thisisnago <yurii.nagorniak@gmail.com>
2024-03-05 23:25:15 +0000

    Fixed path in usePublishRoom

--- modules/web/src/entities/games/ui/usePublishRoom.ts
+++ modules/web/src/entities/games/ui/usePublishRoom.ts
@@ -3,7 +3,7 @@ import { MutationOptions, MutationResponse } from "@/shared/api/types";
 import { AxiosError, AxiosResponse } from "axios";
 import useSWRMutation from "swr/mutation";
 // import { InviteAcceptData, InviteAcceptPayload } from "types";
-import { RoomData } from "../../types";
+import { RoomData } from "../types";
 
 export function usePublishRoom<D = RoomData, P = void>(payload: number, options?: MutationOptions<D, P>): MutationResponse<D, P> {
     return useSWRMutation<AxiosResponse<D>, AxiosError, string, P, D>(
